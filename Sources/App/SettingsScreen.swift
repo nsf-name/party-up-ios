08cd58a27c091536ce3b20a1d2617692c0f3dddc
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2026-06-27 15:43:20 -0400

    host and port checking

--- Sources/App/SettingsScreen.swift
+++ Sources/App/SettingsScreen.swift
@@ -1,5 +1,6 @@
 // SettingsScreen.swift
 import SwiftUI
+import Network
 
 struct SettingsScreen: View {
     @State private var settings = AppSettings.shared
@@ -7,6 +8,7 @@ struct SettingsScreen: View {
     @State private var urlText = ""
     @State private var portText = ""
     
+    // this is very not RFC1123 but it gets the job done
     var parsedURL: URL? {
         URL(string: urlText).flatMap {
             ($0.scheme == "http" || $0.scheme == "https") && $0.host != nil ? $0 : nil
@@ -14,6 +16,7 @@ struct SettingsScreen: View {
     }
     var parsedPort: UInt16? { UInt16(portText) }
     
+    // TODO: it would be really nice to allow the user to set options independently
     var body: some View {
         NavigationStack {
             Group {
@@ -27,20 +30,45 @@ struct SettingsScreen: View {
                     Toggle(isOn: $settings.userPreferences.isVerbose) {
                         Text("Verbose Mode")
                     }.onChange(of: settings.userPreferences.isVerbose) {
+                        if settings.userPreferences.isVerbose {
+                            coreLogger.info("user has enabled verbose mode")
+                        } else {
+                            coreLogger.info("user has disabled verbose mode")
+                        }
                         try? settings.save()
                     }
-                    // TODO: finish up this part
-                    TextField("http://localhost", text: $urlText)
-                        .border(parsedURL == nil && !urlText.isEmpty ? .red : .clear)
-                        .autocorrectionDisabled()
-                        .textInputAutocapitalization(.never)
-                    TextField("Port", text: $portText)
-                        .border(parsedPort == nil && !portText.isEmpty ? .red : .clear)
-                        .keyboardType(.numberPad)
-                    // TODO: error handling on invalid state
-                    Button { try? settings.save() } label: {
+                    HStack {
+                        Text("Host")
+                            .padding(.trailing, 4)
+                        TextField(settings.userPreferences.serverURL.absoluteString, text: $urlText)
+                            .border(parsedURL == nil && !urlText.isEmpty ? .red : .blue)
+                            .autocorrectionDisabled(true)
+                            .textInputAutocapitalization(.never)
+                            .textFieldStyle(.roundedBorder)
+                            .textContentType(.URL)
+                            .padding(.leading, 4)
+                    }
+                    HStack {
+                        Text("Port")
+                            .padding(.trailing, 4)
+                        TextField(settings.userPreferences.serverPort.description, text: $portText)
+                            .border(parsedPort == nil && !portText.isEmpty ? .red : .blue)
+                            .autocorrectionDisabled(true)
+                            .keyboardType(.numberPad)
+                            .textFieldStyle(.roundedBorder)
+                            .padding(.leading, 4)
+                    }
+                    Button {
+                        // we set our validated versions manually
+                        AppSettings.shared.userPreferences.serverURL = parsedURL!
+                        AppSettings.shared.userPreferences.serverPort = parsedPort!
+                        coreLogger.info("user has changed server settings to url: \(parsedURL!) and port: \(parsedPort!)")
+                        try? settings.save()
+                    } label: {
                         Text("Save Server Options")
-                    }.buttonStyle(.glassProminent)
+                    }
+                    .buttonStyle(.glassProminent)
+                    .disabled(parsedURL == nil || parsedPort == nil)
                 }.padding([.leading,.trailing],16)
                 Spacer()
             }
